061b7c892030e465e2b796c7f6f4c254fb449518
Devtron718 <devin.miranda@gmail.com>
2015-11-13 07:45:44 -0800

    add tombs to footer

--- app/assets/javascripts/components/app/footer.js.jsx
+++ app/assets/javascripts/components/app/footer.js.jsx
@@ -4,6 +4,9 @@ var Footer = React.createClass({
       <div className='footer-container'>
         <section className='clearfix'>
           <a  href='https://www.linkedin.com/in/devinmiranda' id='footer-about'>About Devin</a>
+          <img src={'/images/tombstone-white.png'} id='tomb-left' alt="" height='20' />
+          <img src={'/images/tombstone-white.png'} alt="" height='26' />
+          <img src={'/images/tombstone-white.png'} id='tomb-right' alt="" height='20' />
           <a href='https://github.com/Devtron718/ScareBnB' id='footer-github'>Github</a>
         </section>
 
