a43557c3c9a26bbcb3f57ec8740eb9102287a8b0
Devtron718 <devin.miranda@gmail.com>
2015-11-17 18:37:26 -0800

    updated footer imgs

--- app/assets/javascripts/components/app/footer.js.jsx
+++ app/assets/javascripts/components/app/footer.js.jsx
@@ -4,9 +4,9 @@ var Footer = React.createClass({
       <div className='footer-container'>
         <section className='clearfix'>
           <a  href='https://www.linkedin.com/in/devinmiranda' id='footer-about'>About Devin</a>
-          <img src={'/images/tombstone-white.png'} id='tomb-left' alt="" height='20' />
-          <img src={'/images/tombstone-white.png'} alt="" height='26' />
-          <img src={'/images/tombstone-white.png'} id='tomb-right' alt="" height='20' />
+          <img src={'http://res.cloudinary.com/dn7rukqow/image/upload/v1447811048/tombstone-white_nqvq8x.png'} id='tomb-left' alt="" height='20' />
+          <img src={'http://res.cloudinary.com/dn7rukqow/image/upload/v1447811048/tombstone-white_nqvq8x.png'} alt="" height='26' />
+          <img src={'http://res.cloudinary.com/dn7rukqow/image/upload/v1447811048/tombstone-white_nqvq8x.png'} id='tomb-right' alt="" height='20' />
           <a href='https://github.com/Devtron718/ScareBnB' id='footer-github'>Github</a>
         </section>
 
