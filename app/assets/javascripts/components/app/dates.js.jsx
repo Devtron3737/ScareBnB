e3e1ebecbcf67800bb2f8727fd6fd70c34ea2fa9
Devtron718 <devin.miranda@gmail.com>
2015-11-19 20:28:55 -0800

    fixed dates update in listings index

--- app/assets/javascripts/components/app/dates.js.jsx
+++ app/assets/javascripts/components/app/dates.js.jsx
@@ -12,24 +12,69 @@ var Dates = React.createClass({
   },
 
   handleDateFieldChange: function (type, date) {
-    this.changedType = type
-    if (type === "check_in") {
-      // this.onChangeCallback called in this.checkDate
-      this.setState(
-        {check_in: date},
-        this.checkDate
-      )
-      return;
-    } else {
-      // this.onChangeCallback called in this.checkDate
-      this.setState(
-        {check_out: date},
-        this.checkDate
-      )
-      return;
+    // this.changedType = type
+    // these are returning before changecallback is done
+    switch (type) {
+      case "check_in":
+        if (date > this.state.check_out) {
+          this.setState(
+            {
+              check_out: DateUtil.correspondingDay('add', date),
+              check_in: date
+            },
+            function () {
+              this.props.onChangeCallback(this.state.check_in, this.state.check_out)
+            }
+          )
+        } else {
+          this.setState(
+            {check_in: date},
+            function () {
+              this.props.onChangeCallback(this.state.check_in, this.state.check_out)
+            }
+          )
+        }
+        break;
+      case "check_out":
+        if (date < this.state.check_in) {
+          this.setState(
+            {
+              check_in: DateUtil.correspondingDay('subtract', date),
+              check_out: date
+            },
+            function () {
+              this.props.onChangeCallback(this.state.check_in, this.state.check_out)
+            }
+          )
+        } else {
+          this.setState(
+            {check_out: date},
+            function () {
+              this.props.onChangeCallback(this.state.check_in, this.state.check_out)
+            }
+          )
+          this.props.onChangeCallback(this.state.check_in, this.state.check_out)
+        }
     }
 
-    this.onChangeCallback(this.state.check_in, this.state.check_out)
+
+    // if (type === "check_in") {
+    //   // this.onChangeCallback called in this.checkDate
+    //   this.setState(
+    //     {check_in: date},
+    //     this.checkDate
+    //   )
+    //   return;
+    // } else {
+    //   // this.onChangeCallback called in this.checkDate
+    //   this.setState(
+    //     {check_out: date},
+    //     this.checkDate
+    //   )
+    //   return;
+    // }
+    //
+    // this.onChangeCallback(this.state.check_in, this.state.check_out)
   },
 
   checkDate: function (state) {
