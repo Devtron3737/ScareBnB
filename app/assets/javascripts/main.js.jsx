49da49db6b33217b02cf4bbd798371c3651132a9
Devtron718 <devin.miranda@gmail.com>
2015-11-25 14:01:19 -0800

    refactor main.js to check if root is defined

--- app/assets/javascripts/main.js.jsx
+++ app/assets/javascripts/main.js.jsx
@@ -1,29 +1,32 @@
-var Router = ReactRouter.Router,
-    Route = ReactRouter.Route,
-    IndexRoute = ReactRouter.IndexRoute,
-    Link = ReactRouter.Link;
-
-var App = React.createClass({
-  mixins: [ReactRouter.History],
-
-  render: function () {
-      return(
-        <div>
-          {this.props.children}
-        </div>
-      );
-  }
-});
-
-var routes = (
-  <Route name='app' component={App}  path="/">
-    <IndexRoute name='home' component={Home} />
-    <Route name='listings-index' component={ListingsIndex} path="listings/:search" />
-    <Route component={ListingsShow} path="listing/:listingId"/>
-    <Route component={GhostPage} path="user" />
-  </Route>
-);
-
 $( function () {
-  React.render(<Router>{routes}</Router>, document.getElementById('app'));
+  var root = document.getElementById('app'),
+      Router = ReactRouter.Router,
+      Route = ReactRouter.Route,
+      IndexRoute = ReactRouter.IndexRoute,
+      Link = ReactRouter.Link;
+
+  var App = React.createClass({
+    mixins: [ReactRouter.History],
+
+    render: function () {
+        return(
+          <div>
+            {this.props.children}
+          </div>
+        );
+    }
+  });
+
+  var routes = (
+    <Route name='app' component={App}  path="/">
+      <IndexRoute name='home' component={Home} />
+      <Route name='listings-index' component={ListingsIndex} path="listings/:search" />
+      <Route component={ListingsShow} path="listing/:listingId"/>
+      <Route component={GhostPage} path="user" />
+    </Route>
+  );
+
+  if (root) {
+    React.render(<Router>{routes}</Router>, root);
+  }  
 });
