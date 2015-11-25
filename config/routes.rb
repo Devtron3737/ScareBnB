b755ca7d147117ee86dba57cb0cece266d294df7
Devtron718 <devin.miranda@gmail.com>
2015-11-24 20:09:56 -0800

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :sessions
+
 
   namespace :api do
     resources :listings
