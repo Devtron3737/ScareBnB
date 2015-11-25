d607325303d045ef0200479e265479f1bd66a4d5
Devtron718 <devin.miranda@gmail.com>
2015-11-25 15:35:44 -0800

    add upload pic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def new
     render :new
   end
+
+  def create
+    # if valid user creation, then
+    # log new user in and direct to homepage
+  end
 end
