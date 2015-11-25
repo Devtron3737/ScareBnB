55adc82add8f767abc6424e024b6b9c06f530fa1
Devtron718 <devin.miranda@gmail.com>
2015-11-25 09:28:16 -0800

    remove unused methods in use model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < ApplicationController
-  
+  def create
+    # log user in
+  end
+
+  def new
+    # signup
+  end
+
+  def destroy
+    # log out
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,54 +15,8 @@ class User < ActiveRecord::Base
   has_one :picture
 
   has_many(
-    :sent_messages,
-    class_name: "Message",
-    foreign_key: :from_user
-  )
-
-  has_many(
-    :received_messages,
-    class_name: "Message",
-    foreign_key: :to_user
-  )
-
-
-  # as a ghost, you write ratings on a guest associated
-  # with a reservation. As a guest, you write ratings on
-  # a listing.
-  # so on ghost show, well be listing all the reviews associated
-  # with all of their listings
-  # on a users prev trips, clicking a trip will show
-  # all of the ratings associated with that reservation
-  # so a user doesnt need every review theyve written,
-  # just theyre reservations
-
-  has_many(
     :reservations_as_guest,
     class_name: "Reservation",
     foreign_key: :guest_id
   )
-
-  has_many :reservations_as_ghost, through: :listings, source: :reservations
-  has_many :listing_ratings_as_ghost, through: :reservations_as_ghost, source: :listing_ratings
-  has_many :listing_ratings_as_guest, through: :reservations_as_guest, source: :listing_ratings
-  has_many :user_ratings_as_ghost, through: :reservations_as_ghost, source: :user_ratings
-  has_many :user_ratings_as_guest, through: :reservations_as_guest, source: :user_ratings
-
-  def need_action_reservations
-    self.reservations_as_ghost.where("status = 'needs action'")
-  end
-
-  def accepted_reservations_as_ghost
-    self.reservations_as_ghost.where("status = 'approved'")
-  end
-
-  def pending_reservations
-    self.reservations_as_guest.where("status = 'needs action'")
-  end
-
-  def accepted_reservations_as_guest
-    self.reservations_as_guest.where("status = 'approved'")
-  end
-
 end
