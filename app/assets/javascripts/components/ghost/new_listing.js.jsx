6a8a896f5ecbb6e2ed764b0b44ebe65c891bf641
Devtron718 <devin.miranda@gmail.com>
2015-11-29 22:38:00 -0800

    style new listing

--- app/assets/javascripts/components/ghost/new_listing.js.jsx
+++ app/assets/javascripts/components/ghost/new_listing.js.jsx
@@ -127,38 +127,36 @@ var NewListing = React.createClass({
   render: function () {
 
     return(
-      <div id='manage-new-container'>
+      <div>
 
         <div className='manage-block-title'>List Your Haunting</div>
 
-        <ul id='manage-new-list'>
-          <li className='clearfix'>Title <input onChange={this.onTitleChange} type='text' id='manage-new-title' placeholder='ex: Winchester Mystery House' /></li>
-          <li className='clearfix'>
-            Address
+          <div id='manage-new-content'>
+            <input onChange={this.onTitleChange} type='text' className='manage-new-input' id='manage-new-title' placeholder='Title' />
             <input type='text'
+                   className='manage-new-input'
                    id='manage-new-address'
-                   placeholder='ex: 525 S Winchester Blvd, San Jose, CA'
+                   placeholder='Address'
                    onChange={this.handleChange}
                    value={this.state.value}>
             </input>
-          </li>
-          <li className='clearfix'>Description <input type='textfield' id='manage-new-description' placeholder='ex: A horribly confusing abode' /></li>
-          <li className='clearfix'>Toenails <input type='text' id='manage-new-toenails' /></li>
-        </ul>
+            <input type='textfield' className='manage-new-input' id='manage-new-description' placeholder='Description' />
+            <input type='text' className='manage-new-input' id='manage-new-toenails' placeholder='Toenails' />
 
-        <button onClick={this.handlePicUpload}
-                className='button'
-                id='manage-new-picupload' >
-                Upload photo
-        </button>
-        <div>Photo count: {this.state.picCount}</div>
+            <button onClick={this.handlePicUpload}
+                    className='button'
+                    id='manage-new-picupload' >
+                    Upload photo
+            </button>
 
-        <button onClick={this.handleSubmit}
-                className='button'
-                id='manage-new-submit' >
-                List
-        </button>
+            <div id='new-listing-photocount'>Photo count: {this.state.picCount}</div>
 
+            <button onClick={this.handleSubmit}
+                    className='button'
+                    id='manage-new-submit' >
+                    List
+            </button>
+          </div>
       </div>
     )
   }
