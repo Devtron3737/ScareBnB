4fbb54110ac59ece0883facabf0d1edbad78d94a
Devtron718 <devin.miranda@gmail.com>
2015-12-04 11:34:53 -0800

    remove comments

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -11,17 +11,8 @@
 #
 
 class Reservation < ActiveRecord::Base
-  # STATUSES = ['needs action', 'approved', 'rejected']
   validates :listing_id, :check_in, :check_out, :guest_id, presence: true
-  # validates :status, inclusion: STATUSES
   validate :valid_reservation?
-  # Reservation.create!(check_in: Date.new(2015,11,19), check_out: Date.new(2015,11,21),listing_id: 2, guest_id: 2)
-
-  # errors are added as an attribute to
-  # the new record.
-  # so if rsvp = Reservation.create!(...)
-  # rsvp.errors.full_messages gives array
-  # of error statements
 
   belongs_to(
     :guest,
@@ -35,12 +26,6 @@ class Reservation < ActiveRecord::Base
   has_many :user_ratings
 
   def conflicts
-    # Reservation.where([
-    #   "listing_id = :listing_id AND id #{id_matcher} :id",
-    #   {
-    #     listing_id: self.listing_id, id: self.id
-    #   }
-    # ]).where([
     Reservation.where([
       "listing_id = :listing_id
       AND
@@ -53,13 +38,6 @@ class Reservation < ActiveRecord::Base
     ])
   end
 
-# conflicts helper method to get correct syntax for id matching in WHERE
-  # def id_matcher_set(status)
-  #   return 'IS NOT' if status == 'approved'
-  #   '!='
-  # end
-
-# date.new(year, month, day)
   def valid_reservation?
     if conflicts.empty?
       true
@@ -68,9 +46,4 @@ class Reservation < ActiveRecord::Base
       false
     end
   end
-
-  # def update_reservations
-  #   # make sure not to update yourself (the recent reservation creation)
-  #   self.conflicts('needs action').update_all(status: 'rejected')
-  # end
 end
