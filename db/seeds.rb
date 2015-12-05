3f3cd3e5329bc47c8f3d0ae4ed0eb2294a611f94
Devtron718 <devin.miranda@gmail.com>
2015-12-04 22:26:15 -0800

    remove amenities

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,136 +51,136 @@ Picture.create!(user_id: 20, url: 'http://res.cloudinary.com/dn7rukqow/image/upl
 
 Listing.create!(ghost_id: 1, toe_nails: 25, title: "Lovely victorian home",
   address: "111 Madrid St., San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.727635, lng: -122.428021,
+  lat: 37.727635, lng: -122.428021,
   description: "Bloodstained carpets, creaky floorboards, and constantly flickering lightbulbs are just some of the wonderful amenities this property boasts. If guests get bored, feel free to explore the mysterious catacombs underneath the property. It is said there is a lost torture chamber down there. Just beware, the neighboring resident psycho finds his way down there from time to time. Wouldn’t want to run into him now!"
 )
 Listing.create!(ghost_id: 1, toe_nails: 75, title: "Rotten abode near the coast",
   address: "2762 40th Ave., San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.735323, lng: -122.497367,
+  lat: 37.735323, lng: -122.497367,
   description: "Don’t mind the banging coming from the upstairs attic, that has been going on for some time now. As a matter of fact, I would’t dwell too much on any of the unexplainable noises coming from property. The last guest that did pay some mind to it actually ended up losing their mind. We wouldn't want that to happen. Please be sure to leave a nice review at the end of you’re stay."
 )
 Listing.create!(ghost_id: 2, toe_nails: 21, title: "Large space full of spirits",
   address: "2445 Prospect St., Berkeley, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.866250, lng: -122.249629,
+  lat: 37.866250, lng: -122.249629,
   description: "This very haunted space is frequented by quite the upset spirit. It is said that if you’re up deep enough into the night, the howls of La Llorona can clearly be heard from the property. And careful not to be alone on the grounds during nightfall, as several guests have already disappeared due to the Llorona mistaking them for her lost children. The house also has a lovely kitchen."
 )
 Listing.create!(ghost_id: 3, toe_nails: 29, title: "A home away from home",
   address: "824 54th St., Oakland, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.839046, lng: -122.270618,
+  lat: 37.839046, lng: -122.270618,
   description: "Bloodstained carpets, creaky floorboards, and constantly flickering lightbulbs are just some of the wonderful amenities this property boasts. If guests get bored, feel free to explore the mysterious catacombs underneath the property. It is said there is a lost torture chamber down there. Just beware, the neighboring resident psycho finds his way down there from time to time. Wouldn’t want to run into him now!"
 )
 
 Listing.create!(ghost_id: 3, toe_nails: 41, title: "Secluded abode near a dark park",
   address: "222 Willard North, San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.776441, lng: -122.456654,
+  lat: 37.776441, lng: -122.456654,
   description: "Don’t mind the banging coming from the upstairs attic, that has been going on for some time now. As a matter of fact, I would’t dwell too much on any of the unexplainable noises coming from property. The last guest that did pay some mind to it actually ended up losing their mind. We wouldn't want that to happen. Please be sure to leave a nice review at the end of you’re stay."
 )
 
 Listing.create!(ghost_id: 2, toe_nails: 302, title: "The hauntedest of the haunted",
   address: "1700 Irving St., San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.763782, lng: -122.476104,
+  lat: 37.763782, lng: -122.476104,
   description: "This very haunted space is frequented by quite the upset spirit. It is said that if you’re up deep enough into the night, the howls of La Llorona can clearly be heard from the property. And careful not to be alone on the grounds during nightfall, as several guests have already disappeared due to the Llorona mistaking them for her lost children. The house also has a lovely kitchen."
 )
 
 Listing.create!(ghost_id: 4, toe_nails: 32, title: "I wonder what's behind that door...",
   address: "1032 2nd St., Novato, CA",
-  amenities: JSON.generate(["stuff"]), lat: 38.109179, lng: -122.571868,
+  lat: 38.109179, lng: -122.571868,
   description: "Bloodstained carpets, creaky floorboards, and constantly flickering lightbulbs are just some of the wonderful amenities this property boasts. If guests get bored, feel free to explore the mysterious catacombs underneath the property. It is said there is a lost torture chamber down there. Just beware, the neighboring resident psycho finds his way down there from time to time. Wouldn’t want to run into him now!"
 )
 
 Listing.create!(ghost_id: 5, toe_nails: 35, title: "Did you hear that? ",
   address: "1298 Howard St., San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.775699, lng: -122.413250,
+  lat: 37.775699, lng: -122.413250,
   description: "Don’t mind the banging coming from the upstairs attic, that has been going on for some time now. As a matter of fact, I would’t dwell too much on any of the unexplainable noises coming from property. The last guest that did pay some mind to it actually ended up losing their mind. We wouldn't want that to happen. Please be sure to leave a nice review at the end of you’re stay."
 )
 
 Listing.create!(ghost_id: 6, toe_nails: 77, title: "Plenty of things that go bump in the night",
   address: "65 Alpine Ave., Daly City, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.686377, lng: -122.486442,
+  lat: 37.686377, lng: -122.486442,
   description: "This very haunted space is frequented by quite the upset spirit. It is said that if you’re up deep enough into the night, the howls of La Llorona can clearly be heard from the property. And careful not to be alone on the grounds during nightfall, as several guests have already disappeared due to the Llorona mistaking them for her lost children. The house also has a lovely kitchen."
 )
 
 Listing.create!(ghost_id: 7, toe_nails: 102, title: "Ghouls, goblins, and zombies",
   address: "1452 Willard St., San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.763351, lng: -122.453933,
+  lat: 37.763351, lng: -122.453933,
   description: "Bloodstained carpets, creaky floorboards, and constantly flickering lightbulbs are just some of the wonderful amenities this property boasts. If guests get bored, feel free to explore the mysterious catacombs underneath the property. It is said there is a lost torture chamber down there. Just beware, the neighboring resident psycho finds his way down there from time to time. Wouldn’t want to run into him now!"
 )
 
 Listing.create!(ghost_id: 8, toe_nails: 542, title: "You don't want to know what happened here",
   address: "18 Rowe Ranch Way, Novato, CA",
-  amenities: JSON.generate(["stuff"]), lat: 38.071136, lng: -122.543495,
+  lat: 38.071136, lng: -122.543495,
   description: "Don’t mind the banging coming from the upstairs attic, that has been going on for some time now. As a matter of fact, I would’t dwell too much on any of the unexplainable noises coming from property. The last guest that did pay some mind to it actually ended up losing their mind. We wouldn't want that to happen. Please be sure to leave a nice review at the end of you’re stay."
 )
 
 Listing.create!(ghost_id: 9, toe_nails: 22, title: "Beware of demon",
   address: "185 El Rancho Dr., Daly City, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.659941, lng: -122.453407,
+  lat: 37.659941, lng: -122.453407,
   description: "This very haunted space is frequented by quite the upset spirit. It is said that if you’re up deep enough into the night, the howls of La Llorona can clearly be heard from the property. And careful not to be alone on the grounds during nightfall, as several guests have already disappeared due to the Llorona mistaking them for her lost children. The house also has a lovely kitchen."
 )
 
 Listing.create!(ghost_id: 10, toe_nails: 876, title: "Gate of hell in basement",
   address: "1008 Pacific Ave, San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.796152, lng: -122.412128,
+  lat: 37.796152, lng: -122.412128,
   description: "Bloodstained carpets, creaky floorboards, and constantly flickering lightbulbs are just some of the wonderful amenities this property boasts. If guests get bored, feel free to explore the mysterious catacombs underneath the property. It is said there is a lost torture chamber down there. Just beware, the neighboring resident psycho finds his way down there from time to time. Wouldn’t want to run into him now!"
 )
 
 Listing.create!(ghost_id: 11, toe_nails: 43, title: "Not one survivor yet",
   address: "1066 Filbert St., San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.800638, lng: -122.417137,
+  lat: 37.800638, lng: -122.417137,
   description: "Don’t mind the banging coming from the upstairs attic, that has been going on for some time now. As a matter of fact, I would’t dwell too much on any of the unexplainable noises coming from property. The last guest that did pay some mind to it actually ended up losing their mind. We wouldn't want that to happen. Please be sure to leave a nice review at the end of you’re stay."
 )
 
 Listing.create!(ghost_id: 10, toe_nails: 894, title: "Perfect place to rest for eternity",
   address: "740 Cottonwood, South San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.661013, lng: -122.423487,
+  lat: 37.661013, lng: -122.423487,
   description: "This very haunted space is frequented by quite the upset spirit. It is said that if you’re up deep enough into the night, the howls of La Llorona can clearly be heard from the property. And careful not to be alone on the grounds during nightfall, as several guests have already disappeared due to the Llorona mistaking them for her lost children. The house also has a lovely kitchen."
 )
 
 Listing.create!(ghost_id: 13, toe_nails: 21, title: "An evil spirits delight",
   address: "598 Fulton St., San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.778470, lng: -122.426488,
+  lat: 37.778470, lng: -122.426488,
   description: "Bloodstained carpets, creaky floorboards, and constantly flickering lightbulbs are just some of the wonderful amenities this property boasts. If guests get bored, feel free to explore the mysterious catacombs underneath the property. It is said there is a lost torture chamber down there. Just beware, the neighboring resident psycho finds his way down there from time to time. Wouldn’t want to run into him now!"
 )
 
 Listing.create!(ghost_id: 14, toe_nails: 266, title: "Muahahahahaha",
   address: "28 Sonora Ave., South San Francisco, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.645214, lng: -122.425114,
+  lat: 37.645214, lng: -122.425114,
   description: "Don’t mind the banging coming from the upstairs attic, that has been going on for some time now. As a matter of fact, I would’t dwell too much on any of the unexplainable noises coming from property. The last guest that did pay some mind to it actually ended up losing their mind. We wouldn't want that to happen. Please be sure to leave a nice review at the end of you’re stay."
 )
 
 Listing.create!(ghost_id: 15, toe_nails: 342, title: "Who is that in the window?",
   address: "2149 Oregon St., Berkeley, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.858006, lng: -122.264804,
+  lat: 37.858006, lng: -122.264804,
   description: "This very haunted space is frequented by quite the upset spirit. It is said that if you’re up deep enough into the night, the howls of La Llorona can clearly be heard from the property. And careful not to be alone on the grounds during nightfall, as several guests have already disappeared due to the Llorona mistaking them for her lost children. The house also has a lovely kitchen."
 )
 
 Listing.create!(ghost_id: 16, toe_nails: 897, title: "Chainsaw out back",
   address: "2608 9th St., Berkeley, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.858277, lng: -122.291240,
+  lat: 37.858277, lng: -122.291240,
   description: "Bloodstained carpets, creaky floorboards, and constantly flickering lightbulbs are just some of the wonderful amenities this property boasts. If guests get bored, feel free to explore the mysterious catacombs underneath the property. It is said there is a lost torture chamber down there. Just beware, the neighboring resident psycho finds his way down there from time to time. Wouldn’t want to run into him now!"
 )
 
 Listing.create!(ghost_id: 17, toe_nails: 133, title: "You'll never make it out alive",
   address: "55 Montell St., Oakland, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.823980, lng: -122.253474,
+  lat: 37.823980, lng: -122.253474,
   description: "Don’t mind the banging coming from the upstairs attic, that has been going on for some time now. As a matter of fact, I would’t dwell too much on any of the unexplainable noises coming from property. The last guest that did pay some mind to it actually ended up losing their mind. We wouldn't want that to happen. Please be sure to leave a nice review at the end of you’re stay."
 )
 
 Listing.create!(ghost_id: 18, toe_nails: 543, title: "A monsters mash",
   address: "2430 Poplar St., Oakland, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.818285, lng: -122.285918,
+  lat: 37.818285, lng: -122.285918,
   description: "This very haunted space is frequented by quite the upset spirit. It is said that if you’re up deep enough into the night, the howls of La Llorona can clearly be heard from the property. And careful not to be alone on the grounds during nightfall, as several guests have already disappeared due to the Llorona mistaking them for her lost children. The house also has a lovely kitchen."
 )
 
 Listing.create!(ghost_id: 19, toe_nails: 133, title: "Ahhhhh!",
   address: "450 Stow Ave., Oakland, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.803502, lng: -122.250899,
+  lat: 37.803502, lng: -122.250899,
   description: "Bloodstained carpets, creaky floorboards, and constantly flickering lightbulbs are just some of the wonderful amenities this property boasts. If guests get bored, feel free to explore the mysterious catacombs underneath the property. It is said there is a lost torture chamber down there. Just beware, the neighboring resident psycho finds his way down there from time to time. Wouldn’t want to run into him now!"
 )
 
 Listing.create!(ghost_id: 20, toe_nails: 111, title: "Concede to the dark side",
   address: "110 Frisbie St., Oakland, CA",
-  amenities: JSON.generate(["stuff"]), lat: 37.818963, lng: -122.259139,
+  lat: 37.818963, lng: -122.259139,
   description: "Don’t mind the banging coming from the upstairs attic, that has been going on for some time now. As a matter of fact, I would’t dwell too much on any of the unexplainable noises coming from property. The last guest that did pay some mind to it actually ended up losing their mind. We wouldn't want that to happen. Please be sure to leave a nice review at the end of you’re stay."
 )
 
