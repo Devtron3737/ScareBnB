3f9f212658771b2dc85436fa311a2ca4ee244a16
Devtron718 <devin.miranda@gmail.com>
2015-12-10 21:28:46 -0800

    hide env variables

--- app/assets/javascripts/components/app/footer.js.jsx
+++ app/assets/javascripts/components/app/footer.js.jsx
@@ -4,9 +4,9 @@ var Footer = React.createClass({
       <div className='footer-container'>
         <section className='clearfix'>
           <a  href='https://www.linkedin.com/in/devinmiranda' id='footer-about'>About Devin</a>
-          <img src={'http://res.cloudinary.com/dn7rukqow/image/upload/v1447811048/tombstone-white_nqvq8x.png'} id='tomb-left' alt="" height='20' />
-          <img src={'http://res.cloudinary.com/dn7rukqow/image/upload/v1447811048/tombstone-white_nqvq8x.png'} alt="" height='26' />
-          <img src={'http://res.cloudinary.com/dn7rukqow/image/upload/v1447811048/tombstone-white_nqvq8x.png'} id='tomb-right' alt="" height='20' />
+          <img src={'http://res.cloudinary.com/' + window.CLOUDINARY_CLOUD_NAME + '/image/upload/v1447811048/tombstone-white_nqvq8x.png'} id='tomb-left' alt="" height='20' />
+          <img src={'http://res.cloudinary.com/' + window.CLOUDINARY_CLOUD_NAME + '/image/upload/v1447811048/tombstone-white_nqvq8x.png'} alt="" height='26' />
+          <img src={'http://res.cloudinary.com/' + window.CLOUDINARY_CLOUD_NAME + '/image/upload/v1447811048/tombstone-white_nqvq8x.png'} id='tomb-right' alt="" height='20' />
           <a href='https://github.com/Devtron718/ScareBnB' id='footer-github'>Github</a>
         </section>
 
