2475303ad091894b147aad16b7fd7a062ef89628
Devtron718 <devin.miranda@gmail.com>
2016-01-24 21:26:32 -0800

    remove console log

--- app/assets/javascripts/util/cloudinary.js
+++ app/assets/javascripts/util/cloudinary.js
@@ -1,6 +1,5 @@
 var CloudinaryUtil = {
   picUpload: function (callback) {
-    console.log(window.CLOUDINARY_CLOUD_NAME)
     cloudinary.openUploadWidget(
       {
         cloud_name: window.CLOUDINARY_CLOUD_NAME,
