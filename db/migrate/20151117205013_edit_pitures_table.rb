82c95b494d19ea1c91f1b5bdbe78ca84fe38257d
Devtron718 <devin.miranda@gmail.com>
2016-02-22 16:05:06 -0800

    change edit picture col migration

--- db/migrate/20151117205013_edit_pitures_table.rb
+++ db/migrate/20151117205013_edit_pitures_table.rb
@@ -1,9 +1,9 @@
 class EditPituresTable < ActiveRecord::Migration
   def change
-    remove_column :pictures, :image_file_name, :string
-    remove_column :pictures, :image_content_type, :string
-    remove_column :pictures, :image_file_size, :integer
-    remove_column :pictures, :image_updated_at, :datetime
+    # remove_column :pictures, :image_file_name, :string
+    # remove_column :pictures, :image_content_type, :string
+    # remove_column :pictures, :image_file_size, :integer
+    # remove_column :pictures, :image_updated_at, :datetime
     add_column :pictures, :url, :string
   end
 end
